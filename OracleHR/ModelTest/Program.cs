2ac67588f2e8a64ed9e65924983ec792447a9026
Cesar Lopez <cesarl@outlook.com>
2016-03-06 13:55:51 -0700

    Create Console App ModelTest, then use HRLibraary to display EMployee infor through EF and LinQ

---
+++ OracleHR/ModelTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLibrary;
+
+namespace ModelTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hr = new Entities();
+
+            var employees =
+                hr.EMPLOYEES.Where(e => e.LAST_NAME.StartsWith("P")).OrderBy(e => e.LAST_NAME).ThenBy(e => e.FIRST_NAME);
+
+            Console.WriteLine("Selected Products:");
+            Console.WriteLine("------------------");
+            Console.WriteLine("       Employee         Salary");
+            Console.WriteLine("--------------------  -------------");
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine("{0,-22} {1:C}", emp.LAST_NAME + ", " + emp.FIRST_NAME, emp.SALARY);
+            }
+        }
+    }
+}
