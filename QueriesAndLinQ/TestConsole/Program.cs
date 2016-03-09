175acab9ca8fccbe85653664c292000fba03ee2c
Cesar Lopez <cesarl@outlook.com>
2016-03-09 16:12:48 -0700

    QueriesAndLinQ VerySimple and rojection queries using Oracle Managed Data Access and EF

---
+++ QueriesAndLinQ/TestConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = new Queries();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("A: Very Simple Query");
+                Console.WriteLine("B: Projection Query");
+
+                Console.WriteLine("\nSelect an option ('.' to exit...)");
+
+                var key = Char.ToUpper(Console.ReadKey().KeyChar);
+                
+                if (key == '.')
+                    return;
+
+                Console.Clear();
+
+                switch (key)
+                {
+                    case 'A':
+                        q.VerySimpleQuery();
+                        break;
+                    case 'B':
+                        q.ProjectionQuery();
+                        break;
+                }
+
+                if (Console.ReadKey(true).KeyChar == '.')
+                    return;
+            }
+
+        }
+    }
+}
---
+++ QueriesAndLinQ/TestConsole/Queries.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using HRLibrary;
+
+namespace TestConsole
+{
+    public class Queries
+    {
+        public void VerySimpleQuery()
+        {
+            var dc = new HREntities();
+
+            var employees = from emp in dc.EMPLOYEES
+                where emp.DEPARTMENT_ID == 90
+                select emp;
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine("{0} {1}", emp.FIRST_NAME, emp.LAST_NAME);
+            }
+
+        }
+
+        public void ProjectionQuery()
+        {
+            var dc = new HREntities();
+
+            var employees = dc.EMPLOYEES
+                .Where(e => e.DEPARTMENT_ID == 100)
+                .OrderBy(e => e.LAST_NAME)
+                .ThenBy(e => e.FIRST_NAME)
+                .Select(e => new{ e.FIRST_NAME, e.LAST_NAME, e.EMAIL, e.PHONE_NUMBER});
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine("{0} {1} - {2}", emp.FIRST_NAME, emp.LAST_NAME, emp.EMAIL);
+            }
+
+        }
+    }
+}
